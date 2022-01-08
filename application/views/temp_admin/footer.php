9eb8b4d0f03674be8957e68d97924259647617ae
Christuajisatrio01 <satriobastano@gmail.com>
2022-01-08 21:01:20 +0800

    tugas testing

---
+++ application/views/temp_admin/footer.php
@@ -0,0 +1,4 @@
+<div class="pull-right hidden-xs ">
+	Design By Yusda Helmani, S.Kom<br>
+</div>
+<strong>Copyright &copy; <?php echo date("Y"); ?></strong> Hak Cipta Dilindungi Undang-undang. | Repost by <a href="https://stokcoding.com/" title="StokCoding.com" target="_blank">StokCoding.com</a><br>
